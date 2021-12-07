f25d5f0ab0f23faa04c34b03946f6cc6451eb41d
Jessica Choe <jessj.choe@gmail.com>
2021-12-06 21:44:23 -0800

    create Award class and add methods for updating quality

--- award.rb
+++ award.rb
@@ -1,1 +1,56 @@
-Award = Struct.new(:name, :expires_in, :quality)
+class Award
+  attr_accessor :expires_in, :quality
+  attr_reader :name
+
+  def initialize(name, expires_in, quality)
+    @name = name
+    @expires_in = expires_in
+    @quality = quality
+  end
+
+  def blue_first
+    if @quality < 49 && @expires_in <= 0
+        @quality += 2
+    elsif @quality < 50 
+        @quality += 1
+    end
+  end
+
+  def blue_compare
+    if @quality < 50
+      if @expires_in < 0
+        @quality = 0
+      elsif @expires_in < 6
+        @quality += 3
+      elsif @expires_in < 11
+        @quality += 2
+      else
+        @quality += 1
+      end
+    end
+  end
+
+  def blue_star
+    if @quality > 0
+      if @expires_in < 0
+        @quality -= 4
+      else
+        @quality -= 2
+      end
+    end
+  end
+
+  def blue_distinction_plus
+    @quality = 80
+  end
+
+  def normal_award
+    if @quality > 0
+      if @expires_in <= 0
+        @quality -= 2
+      else
+        @quality -= 1
+      end
+    end
+  end
+end
--- update_quality.rb
+++ update_quality.rb
@@ -2,48 +2,78 @@ require 'award'
 
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
-    else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
-    end
     if award.name != 'Blue Distinction Plus'
       award.expires_in -= 1
     end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        if award.quality < 50
-          award.quality += 1
-        end
-      end
+
+    case award.name
+    when 'Blue First'
+      award.blue_first
+    when 'Blue Compare'
+      award.blue_compare
+    when 'Blue Star'
+      award.blue_star
+    when 'Blue Distinction Plus'
+      award.blue_distinction_plus
+    else 
+      award.normal_award
     end
   end
 end
+
+# class Awards
+#   def initialize(expires_in, quality)
+#     @expires_in = (expires_in)
+#     @quality = quality
+#   end
+
+#   def quality
+#     @quality 
+#   end
+  
+#   def blue_first
+#     if @quality < 49 && @expires_in <= 0
+#         @quality += 2
+#     elsif @quality < 50 
+#         @quality += 1
+#     end
+#   end
+
+#   def blue_compare
+#     if @quality < 50
+#       if @expires_in < 0
+#         @quality = 0
+#       elsif @expires_in < 6
+#         @quality += 3
+#       elsif @expires_in < 11
+#         @quality += 2
+#       else
+#         @quality += 1
+#       end
+#     end
+#   end
+
+#   def blue_star
+#     if @quality > 0
+#       if @expires_in < 0
+#         @quality -= 4
+#       else
+#         @quality -= 2
+#       end
+#     end
+#   end
+
+#   def blue_distinction_plus
+#     @quality = 80
+#   end
+
+#   def normal_award
+#     if @quality > 0
+#       if @expires_in <= 0
+#         @quality -= 2
+#       else
+#         @quality -= 1
+#       end
+#     end
+#   end
+# end
